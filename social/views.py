9dad1d55b768ead23469b0febfef343825047b4b
Ács Attila <attilaacs4@gmail.com>
2026-03-16 16:31:41 +0100

    weboldal mukodik

--- social/views.py
+++ social/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'posts': posts})
