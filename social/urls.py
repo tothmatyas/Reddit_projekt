01c95bd2bc32bc16060834625d50d340791855db
Ács Attila <attilaacs4@gmail.com>
2026-03-16 16:36:59 +0100

    poszt view

--- social/urls.py
+++ social/urls.py
@@ -4,3 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new/', views.new_post, name='new_post'),
+]
--- social/views.py
+++ social/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import PostForm  # később
 
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'home.html', {'posts': posts})
+
+@login_required
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
